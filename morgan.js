b0b39d620980821358fcc48cdb9d3caec4a03faa
manisha7343 <manishasharma7343@gmail.com>
2026-04-30 11:43:50 +0530

    updated morgan

--- morgan.js
+++ morgan.js
@@ -1,29 +1,53 @@
-const { log } = require('console');
-const express = require('express')
-const morgan = require('morgan')
+
+
+const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
-//------------------------------------------
 
-//use morgan
-// app.use(morgan('dev'))
 
-//minimal info
+//minimal info----------
 // app.use(morgan('tiny'));
 
-//combined Api browser
-app.use(morgan('combined'));
+//combined Api browser-------
+// app.use(morgan('combined'));
 
-//cutomer los
-// app.use(morgan(':method :url :status :response-time ms'));
+//short and colored logs
+// app.use(morgan('dev'));
 
-//------------------------------------------
+//common logs---------
+// app.use(morgan('common'));
 
-app.get('/', (req,res)=>{
-    res.send('Hello');
+//short logs-------
+// app.use(morgan('short'));
+
+
+//user middleware (dummy for now)----------
+// app.use((req, res, next) => {
+//   req.user = { id: 'user_123' }; // real me JWT se aata hai
+//   next();
+// });
+
+
+// custom logger---------
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
+
+
+// //token---------
+// morgan.token('userId', (req) => req.user?.id || 'guest');
+// app.use(morgan(':method :url :status :userId'));
+
+
+// app.use(morgan('dev', {
+//   skip: (req, res) => res.statusCode < 400
+// }));
+
+
+
+app.get('/', (req, res) => {
+  res.send('Hello');
 });
 
-app.listen(3000, ()=>{
-    console.log('server is runing');
+app.listen(3000, () => {
+  console.log('server is running');
 });
-
